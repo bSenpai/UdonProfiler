da4541ee93dbb54510e5c11d0ccfcc6fa9837bb5
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2023-07-27 17:19:24 -0400

    Create unity profiler and demo sccene. Closes #1.

---
+++ UdonProfilerDemo/Assets/UdonProfilerExampleScene/GameManager.cs
@@ -0,0 +1,91 @@
+﻿using bSenpai.UdonProfiler;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace bSenpai
+{
+
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class GameManager : UdonSharpBehaviour
+    {
+        private Profiler m_Profiler = null;
+
+        void Start()
+        {
+            var profilerGo = GameObject.Find("Profiler");
+            if (profilerGo == null)
+            {
+                Debug.LogError("Profiler object not found in scene!");
+                return;
+            }
+
+            m_Profiler = profilerGo.GetComponent<Profiler>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (m_Profiler)
+            {
+                m_Profiler.BeginSample("FixedUpdate");
+
+                m_Profiler.BeginSample("Loop A");
+                PrintLoop("Hello Loop A", 10);
+                m_Profiler.EndSample();
+
+                m_Profiler.EndSample();
+            }
+        }
+
+        private void Update()
+        {
+            if (m_Profiler)
+            {
+                m_Profiler.BeginSample("Update");
+
+                m_Profiler.BeginSample("Loop B");
+                PrintLoop("Hello Loop B", 10);
+                m_Profiler.EndSample();
+
+                m_Profiler.EndSample();
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if (m_Profiler)
+            {
+                m_Profiler.BeginSample("LateUpdate");
+
+                m_Profiler.BeginSample("Loop C");
+                PrintLoop("Hello Loop C", 10);
+                m_Profiler.EndSample();
+
+                m_Profiler.EndSample();
+            }
+        }
+
+        public override void PostLateUpdate()
+        {
+            if (m_Profiler)
+            {
+                m_Profiler.BeginSample("PostLateUpdate");
+
+                m_Profiler.BeginSample("Loop D");
+                PrintLoop("Hello Loop D", 10);
+                m_Profiler.EndSample();
+
+                m_Profiler.EndSample();
+            }
+        }
+
+        private void PrintLoop(string msg, int iterations)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                Debug.Log(msg);
+            }
+        }
+    }
+}
---
+++ UdonProfilerDemo/Assets/bSenpai/UdonProfiler/Handler.cs
@@ -0,0 +1,28 @@
+﻿// MIT License
+// Copyright (c) 2021 Merlin
+// Source: https://gist.github.com/MerlinVR/2da80b29361588ddb556fd8d3f3f47b5
+
+using UdonSharp;
+using UnityEngine;
+
+namespace bSenpai.UdonProfiler
+{
+    // Have profiler end frame timing after all scripts run.
+    [DefaultExecutionOrder(2_000_000_000)]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class Handler : UdonSharpBehaviour
+    {
+        private Profiler m_Profiler;
+
+        private void Start()
+        {
+            m_Profiler = GetComponent<Profiler>();
+        }
+
+        // End frame at latest time possible.
+        public override void PostLateUpdate()
+        {
+            m_Profiler.EndFrame();
+        }
+    }
+}
